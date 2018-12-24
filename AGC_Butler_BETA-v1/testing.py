0214b44cd2623b3e1cd3210766a67079eec0a2d5
Emile Dreyer <39258693+bot-steev@users.noreply.github.com>
2018-12-24 14:40:16 +0200

    Delete testing.py

--- AGC_Butler_BETA-v1/testing.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-conn = MySQLdb.connect("localhost", "butler", "Zer045kin!", "agc_butler")
-cursor = conn.cursor()
-
-cursor.execute("SHOW TABLES")
-data=cursor.fetchone()
-
-print(data)
